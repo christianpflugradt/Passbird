81efa06532c5e816d03fd7268888aaca34f03274
Christian Pflugradt <christian@pflugradts.de>
2023-09-10 12:06:36 +0200

    test: fixed tests for top level function using mockkstatic

--- src/test/kotlin/de/pflugradts/passbird/application/MainTest.kt
+++ src/test/kotlin/de/pflugradts/passbird/application/MainTest.kt
@@ -3,7 +3,11 @@ package de.pflugradts.passbird.application
 import de.pflugradts.passbird.application.boot.bootModule
 import de.pflugradts.passbird.application.boot.launcher.LauncherModule
 import de.pflugradts.passbird.application.configuration.ReadableConfiguration
+import io.mockk.every
+import io.mockk.mockkStatic
+import io.mockk.unmockkAll
 import io.mockk.verify
+import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import strikt.api.expectThat
@@ -15,6 +19,13 @@ internal class MainTest {
     @BeforeEach
     fun setup() {
         System.clearProperty(ReadableConfiguration.CONFIGURATION_SYSTEM_PROPERTY)
+        mockkStatic(::bootModule)
+        every { bootModule(any(LauncherModule::class)) } returns Unit
+    }
+
+    @AfterEach
+    fun cleanup() {
+        unmockkAll()
     }
 
     @Test
@@ -26,7 +37,7 @@ internal class MainTest {
         main(arrayOf(configurationDirectory))
 
         // then
-        verify(exactly = 1) { bootModule(eq(LauncherModule())) }
+        verify(exactly = 1) { bootModule(any(LauncherModule::class)) }
         expectThat(System.getProperty(ReadableConfiguration.CONFIGURATION_SYSTEM_PROPERTY)) isEqualTo configurationDirectory
     }
 
@@ -36,7 +47,7 @@ internal class MainTest {
         main(emptyArray())
 
         // then
-        verify(exactly = 1) { bootModule(eq(LauncherModule())) }
+        verify(exactly = 1) { bootModule(any(LauncherModule::class)) }
         expectThat(System.getProperty(ReadableConfiguration.CONFIGURATION_SYSTEM_PROPERTY)).isNullOrEmpty()
     }
 }
