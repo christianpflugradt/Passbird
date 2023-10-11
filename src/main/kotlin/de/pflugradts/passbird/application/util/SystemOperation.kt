b904c1a85954c62022ebaba7ac730a38852c3fa8
Christian Pflugradt <christian@pflugradts.de>
2023-10-11 18:11:41 +0200

    refactor: removed unused functions and obsolete tests

--- src/main/kotlin/de/pflugradts/passbird/application/util/SystemOperation.kt
+++ src/main/kotlin/de/pflugradts/passbird/application/util/SystemOperation.kt
@@ -3,10 +3,8 @@ package de.pflugradts.passbird.application.util
 import de.pflugradts.passbird.domain.model.transfer.Bytes
 import de.pflugradts.passbird.domain.model.transfer.Bytes.Companion.bytesOf
 import de.pflugradts.passbird.domain.model.transfer.Bytes.Companion.emptyBytes
-import java.awt.Desktop
 import java.awt.Toolkit
 import java.awt.datatransfer.StringSelection
-import java.io.File
 import java.io.IOException
 import java.io.InputStream
 import java.io.OutputStream
@@ -26,7 +24,6 @@ private const val JCEKS_KEYSTORE = "JCEKS"
 class SystemOperation {
     val isConsoleAvailable: Boolean get() = System.console() != null
     val jceksInstance: KeyStore get() = KeyStore.getInstance(JCEKS_KEYSTORE)
-    val desktop: Desktop get() = Desktop.getDesktop()
 
     fun readPasswordFromConsole(): CharArray = System.console().readPassword()
     fun resolvePath(directory: String, fileName: String): Path? =
@@ -38,8 +35,5 @@ class SystemOperation {
     fun writeBytesToFile(path: Path, bytes: Bytes): Path = Files.write(path, bytes.toByteArray())
     fun readBytesFromFile(path: Path) = try { bytesOf(Files.readAllBytes(path)) } catch (ex: IOException) { emptyBytes() }
     fun copyToClipboard(text: String) = StringSelection(text).let { Toolkit.getDefaultToolkit().systemClipboard.setContents(it, it) }
-    fun getResourceAsBytes(resource: String) =
-        Thread.currentThread().getContextClassLoader().getResourceAsStream(resource)?.readAllBytes()?.let { bytesOf(it) } ?: emptyBytes()
-    fun openFile(file: File) = if (file.getName().endsWith(".html")) desktop.browse(file.toURI()) else desktop.open(file)
     fun exit() { exitProcess(0) }
 }
