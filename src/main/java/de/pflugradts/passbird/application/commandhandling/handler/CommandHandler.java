4fc88e8adb759884a56194d035118fe69708a30e
Christian Pflugradt <christian@pflugradts.de>
2023-10-15 10:07:48 +0200

    refactor: migrated CommandHandler interface to kotlin

--- src/main/java/de/pflugradts/passbird/application/commandhandling/handler/CommandHandler.java
+++
@@ -1,6 +0,0 @@
-package de.pflugradts.passbird.application.commandhandling.handler;
-
-import de.pflugradts.passbird.domain.service.eventhandling.EventHandler;
-
-public interface CommandHandler extends EventHandler {
-}
