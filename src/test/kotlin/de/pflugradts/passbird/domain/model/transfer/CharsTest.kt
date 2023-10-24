8393a63231d7c635e58d9742524631e18af888d0
Christian Pflugradt <christian@pflugradts.de>
2023-10-24 17:04:35 +0200

    test: added equality tests for Chars

--- src/test/kotlin/de/pflugradts/passbird/domain/model/transfer/CharsTest.kt
+++ src/test/kotlin/de/pflugradts/passbird/domain/model/transfer/CharsTest.kt
@@ -2,10 +2,13 @@ package de.pflugradts.passbird.domain.model.transfer
 
 import de.pflugradts.passbird.domain.model.transfer.Bytes.Companion.emptyBytes
 import de.pflugradts.passbird.domain.model.transfer.Chars.Companion.charsOf
+import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 import strikt.api.expectThat
 import strikt.assertions.isEqualTo
+import strikt.assertions.isFalse
 import strikt.assertions.isNotEqualTo
+import strikt.assertions.isTrue
 
 class CharsTest {
     @Test
@@ -83,6 +86,77 @@ class CharsTest {
 
         // then
         expectThat(givenCharArray) isNotEqualTo referenceCharArray
+    }
+
+    @Nested
+    inner class EqualsTest {
+
+        @Test
+        fun `should be equal to itself`() {
+            // given
+            val chars1 = charsOf(charArrayOf('a', 'b', 'c'))
+            val chars2 = chars1
+
+            // when
+            val actual = chars1.equals(chars2)
+
+            // then
+            expectThat(actual).isTrue()
+        }
+
+        @Test
+        fun `should be equal to chars with equal char array`() {
+            // given
+            val charArray = charArrayOf('a', 'b', 'c')
+            val chars1 = charsOf(charArray)
+            val chars2 = charsOf(charArray)
+
+            // when
+            val actual = chars1.equals(chars2)
+
+            // then
+            expectThat(actual).isTrue()
+        }
+
+        @Test
+        fun `should not be equal to chars with other char array`() {
+            // given
+            val charArray = charArrayOf('a', 'b', 'c')
+            val otherCharArray = charArrayOf('a', 'b', 'd')
+            val chars1 = charsOf(charArray)
+            val chars2 = charsOf(otherCharArray)
+
+            // when
+            val actual = chars1.equals(chars2)
+
+            // then
+            expectThat(actual).isFalse()
+        }
+
+        @Test
+        fun `should not be equal to other class`() {
+            // given
+            val charArray = charArrayOf('a', 'b', 'c')
+            val chars = charsOf(charArray)
+
+            // when
+            val actual = chars.equals(charArray)
+
+            // then
+            expectThat(actual).isFalse()
+        }
+
+        @Test
+        fun `should not be equal to null`() {
+            // given
+            val chars = charsOf(charArrayOf('a', 'b', 'c'))
+
+            // when
+            val actual = chars.equals(null)
+
+            // then
+            expectThat(actual).isFalse()
+        }
     }
 
     private fun charsOf(vararg chars: Char) = charsOf(chars)
