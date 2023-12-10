39ce3334ea22375850f992667f8353e90f6be9ed
Christian Pflugradt <dev@pflugradts.de>
2023-12-10 13:42:36 +0100

    refactor: moved challenge alias function to CommonPasswordServiceCapabilities

--- src/main/kotlin/de/pflugradts/passbird/domain/service/password/CommonPasswordServiceCapabilities.kt
+++ src/main/kotlin/de/pflugradts/passbird/domain/service/password/CommonPasswordServiceCapabilities.kt
@@ -2,13 +2,16 @@ package de.pflugradts.passbird.domain.service.password
 
 import de.pflugradts.passbird.domain.model.event.PasswordEntryNotFound
 import de.pflugradts.passbird.domain.model.namespace.NamespaceSlot
+import de.pflugradts.passbird.domain.model.password.InvalidKeyException
 import de.pflugradts.passbird.domain.model.password.PasswordEntry
 import de.pflugradts.passbird.domain.model.transfer.Bytes
+import de.pflugradts.passbird.domain.model.transfer.CharValue.Companion.charValueOf
 import de.pflugradts.passbird.domain.service.eventhandling.EventRegistry
 import de.pflugradts.passbird.domain.service.password.PasswordService.EntryNotExistsAction
 import de.pflugradts.passbird.domain.service.password.encryption.CryptoProvider
 import de.pflugradts.passbird.domain.service.password.storage.PasswordEntryRepository
 import java.util.Optional
+import java.util.function.Predicate
 
 abstract class CommonPasswordServiceCapabilities(
     private val cryptoProvider: CryptoProvider,
@@ -23,6 +26,17 @@ abstract class CommonPasswordServiceCapabilities(
     fun processEventsAndSync() {
         eventRegistry.processEvents()
         passwordEntryRepository.sync()
+    }
+
+    fun challengeAlias(bytes: Bytes) {
+        if (charValueOf(bytes.getByte(0)).isDigit || anyMatch(bytes.copy()) { charValueOf(it).isSymbol }) {
+            throw InvalidKeyException(bytes)
+        }
+    }
+    private fun anyMatch(bytes: Bytes, predicate: Predicate<Byte>): Boolean {
+        val result = bytes.stream().anyMatch(predicate)
+        bytes.scramble()
+        return result
     }
 
     fun entryExists(keyBytes: Bytes, namespace: NamespaceSlot) = find(encrypted(keyBytes), namespace).isPresent
