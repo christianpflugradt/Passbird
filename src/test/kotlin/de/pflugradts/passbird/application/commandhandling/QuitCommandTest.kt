ed3551b4d7e0f80a074fc1fc263ca5ebb8a5e4b8
Christian Pflugradt <dev@pflugradts.de>
2024-02-03 11:56:55 +0100

    test: added test for quit command with quit reason inactivity

--- src/test/kotlin/de/pflugradts/passbird/application/commandhandling/QuitCommandTest.kt
+++ src/test/kotlin/de/pflugradts/passbird/application/commandhandling/QuitCommandTest.kt
@@ -1,33 +1,74 @@
 package de.pflugradts.passbird.application.commandhandling
 
+import de.pflugradts.kotlinextensions.CapturedOutputPrintStream.Companion.captureSystemOut
 import de.pflugradts.passbird.INTEGRATION
-import de.pflugradts.passbird.application.UserInterfaceAdapterPort
+import de.pflugradts.passbird.adapter.userinterface.CommandLineInterfaceService
+import de.pflugradts.passbird.application.commandhandling.command.QuitCommand
+import de.pflugradts.passbird.application.commandhandling.command.QuitReason.INACTIVITY
 import de.pflugradts.passbird.application.commandhandling.handler.QuitCommandHandler
+import de.pflugradts.passbird.application.configuration.Configuration
+import de.pflugradts.passbird.application.configuration.fakeConfiguration
 import de.pflugradts.passbird.application.util.SystemOperation
+import de.pflugradts.passbird.application.util.fakeSystemOperation
 import de.pflugradts.passbird.domain.model.shell.Shell.Companion.shellOf
 import de.pflugradts.passbird.domain.model.transfer.Input.Companion.inputOf
 import io.mockk.mockk
 import io.mockk.verify
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Tag
 import org.junit.jupiter.api.Test
+import strikt.api.expectThat
+import strikt.assertions.contains
 
 @Tag(INTEGRATION)
 class QuitCommandTest {
 
-    private val userInterfaceAdapterPort = mockk<UserInterfaceAdapterPort>(relaxed = true)
+    private val configuration = mockk<Configuration>()
+    private val commandLineInterfaceService = CommandLineInterfaceService(mockk(), configuration)
     private val systemOperation = mockk<SystemOperation>()
-    private val quitCommandHandler = QuitCommandHandler(userInterfaceAdapterPort, systemOperation)
-    private val inputHandler = createInputHandlerFor(quitCommandHandler)
+    private val quitCommandHandler = QuitCommandHandler(commandLineInterfaceService, systemOperation)
 
-    @Test
-    fun `should handle quit command`() {
-        // given
-        val input = inputOf(shellOf("q"))
+    @BeforeEach
+    fun setup() {
+        fakeConfiguration(instance = configuration)
+        fakeSystemOperation(instance = systemOperation)
+    }
 
-        // when
-        inputHandler.handleInput(input)
+    @Nested
+    inner class UserTest {
+        private val inputHandler = createInputHandlerFor(quitCommandHandler)
 
-        // then
-        verify(exactly = 1) { systemOperation.exit() }
+        @Test
+        fun `should handle quit command`() {
+            // given
+            val input = inputOf(shellOf("q"))
+            val captureSystemOut = captureSystemOut()
+
+            // when
+            captureSystemOut.during { inputHandler.handleInput(input) }
+
+            // then
+            verify(exactly = 1) { systemOperation.exit() }
+            expectThat(captureSystemOut.capture).not().contains("Terminating Passbird due to inactivity")
+        }
+    }
+
+    @Nested
+    inner class InactivityTest {
+        private val commandBus = CommandBus(setOf(quitCommandHandler))
+
+        @Test
+        fun `should output inactivity hint if quit reason is inactivity`() {
+            // given
+            val captureSystemOut = captureSystemOut()
+
+            // when
+            captureSystemOut.during { commandBus.post(QuitCommand(quitReason = INACTIVITY)) }
+
+            // then
+            verify(exactly = 1) { systemOperation.exit() }
+            expectThat(captureSystemOut.capture) contains "Terminating Passbird due to inactivity"
+        }
     }
 }
