17fa19b5258e4ee03e4641926151ab99788ccd6a
Christian Pflugradt <dev@pflugradts.de>
2024-02-24 08:53:57 +0100

    test: added test for protein info command handler

---
+++ src/test/kotlin/de/pflugradts/passbird/application/commandhandling/ProteinInfoCommandTest.kt
@@ -0,0 +1,41 @@
+package de.pflugradts.passbird.application.commandhandling
+
+import de.pflugradts.kotlinextensions.CapturedOutputPrintStream
+import de.pflugradts.passbird.INTEGRATION
+import de.pflugradts.passbird.adapter.userinterface.CommandLineInterfaceService
+import de.pflugradts.passbird.application.commandhandling.capabilities.CanPrintInfo
+import de.pflugradts.passbird.application.commandhandling.handler.ProteinInfoCommandHandler
+import de.pflugradts.passbird.application.configuration.Configuration
+import de.pflugradts.passbird.application.configuration.fakeConfiguration
+import de.pflugradts.passbird.domain.model.shell.Shell.Companion.shellOf
+import de.pflugradts.passbird.domain.model.transfer.Input.Companion.inputOf
+import io.mockk.mockk
+import org.junit.jupiter.api.Tag
+import org.junit.jupiter.api.Test
+import strikt.api.expectThat
+import strikt.assertions.contains
+
+@Tag(INTEGRATION)
+class ProteinInfoCommandTest {
+
+    private val configuration = mockk<Configuration>()
+    private val commandLineInterfaceService = CommandLineInterfaceService(mockk(), configuration)
+    private val proteinInfoCommandHandler = ProteinInfoCommandHandler(CanPrintInfo(), commandLineInterfaceService)
+    private val inputHandler = createInputHandlerFor(proteinInfoCommandHandler)
+
+    @Test
+    fun `should print info`() {
+        // given
+        val input = inputOf(shellOf("p?"))
+        fakeConfiguration(instance = configuration)
+        val captureSystemOut = CapturedOutputPrintStream.captureSystemOut()
+
+        // when
+        captureSystemOut.during {
+            inputHandler.handleInput(input)
+        }
+
+        // then
+        expectThat(captureSystemOut.capture) contains "Available Protein commands"
+    }
+}
