f61912cd47686da4f49aaf4c4219e4e2da60e43d
Christian Pflugradt <110765548+christianpflugradt@users.noreply.github.com>
2024-12-23 10:47:33 +0100

    refactor: move index require logic to its own function

--- src/main/kotlin/de/pflugradts/passbird/domain/model/slot/Slots.kt
+++ src/main/kotlin/de/pflugradts/passbird/domain/model/slot/Slots.kt
@@ -1,6 +1,7 @@
 package de.pflugradts.passbird.domain.model.slot
 
 import de.pflugradts.kotlinextensions.MutableOption
+import de.pflugradts.kotlinextensions.MutableOption.Companion.mutableOptionOf
 import de.pflugradts.kotlinextensions.Option
 import de.pflugradts.passbird.domain.model.slot.Slot.DEFAULT
 import de.pflugradts.passbird.domain.model.slot.Slot.S1
@@ -15,14 +16,12 @@ import de.pflugradts.passbird.domain.model.slot.Slot.S9
 
 open class Slots<T> : Iterable<MutableOption<T>> {
 
-    private val items: Array<MutableOption<T>> = Array(10) { MutableOption.mutableOptionOf() }
+    private val items: Array<MutableOption<T>> = Array(10) { mutableOptionOf() }
 
-    operator fun get(index: Int) = require(index in 0 until 10) { "Index must be between 0 and 9" }
-        .run { items[index] }
+    operator fun get(index: Int) = requireValidIndex(index).run { items[index] }
     operator fun get(slot: Slot) = get(slot.index())
 
-    operator fun set(index: Int, value: T) = require(index in 0 until 10) { "Index must be between 0 and 9" }
-        .run { items[index].set(value) }
+    operator fun set(index: Int, value: T) = requireValidIndex(index).run { items[index].set(value) }
     operator fun set(slot: Slot, value: T) = set(slot.index(), value)
     operator fun set(index: Int, valueOption: Option<T>) = valueOption.ifPresent { set(index, it) }
     operator fun set(slot: Slot, valueOption: Option<T>) = set(slot.index(), valueOption)
@@ -37,5 +36,7 @@ open class Slots<T> : Iterable<MutableOption<T>> {
         fun slotIterator(): Iterable<Slot> = slots
     }
 }
+
+private fun requireValidIndex(index: Int) = require(index in 0 until 10) { "Index must be between 0 and 9" }
 
 private val slots: Iterable<Slot> = listOf(DEFAULT, S1, S2, S3, S4, S5, S6, S7, S8, S9)
