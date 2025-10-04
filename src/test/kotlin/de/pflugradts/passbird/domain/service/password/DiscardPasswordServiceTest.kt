f57f1954c5c97b6817ab3abccddd91968c9ddecb
Christian Pflugradt <110765548+christianpflugradt@users.noreply.github.com>
2025-10-04 12:54:25 +0200

    test(domain): add discard protein tests

--- src/test/kotlin/de/pflugradts/passbird/domain/service/password/DiscardPasswordServiceTest.kt
+++ src/test/kotlin/de/pflugradts/passbird/domain/service/password/DiscardPasswordServiceTest.kt
@@ -3,13 +3,16 @@ package de.pflugradts.passbird.domain.service.password
 import de.pflugradts.passbird.application.security.fakeCryptoProvider
 import de.pflugradts.passbird.domain.model.egg.createEggForTesting
 import de.pflugradts.passbird.domain.model.event.EggNotFound
+import de.pflugradts.passbird.domain.model.shell.EncryptedShell
 import de.pflugradts.passbird.domain.model.shell.Shell.Companion.shellOf
 import de.pflugradts.passbird.domain.model.shell.fakeDec
 import de.pflugradts.passbird.domain.model.shell.fakeEnc
+import de.pflugradts.passbird.domain.model.slot.Slot
 import de.pflugradts.passbird.domain.service.eventhandling.EventRegistry
 import de.pflugradts.passbird.domain.service.password.encryption.CryptoProvider
 import de.pflugradts.passbird.domain.service.password.tree.EggRepository
 import de.pflugradts.passbird.domain.service.password.tree.fakeEggRepository
+import io.mockk.Called
 import io.mockk.mockk
 import io.mockk.slot
 import io.mockk.verify
@@ -41,6 +44,7 @@ class DiscardPasswordServiceTest {
 
         // then
         verify(exactly = 1) { eventRegistry.processEvents() }
+        verify(exactly = 1) { eggRepository.sync() }
         expectThat(givenEgg.viewPassword()) isNotEqualTo givenPassword.fakeEnc()
     }
 
@@ -62,5 +66,67 @@ class DiscardPasswordServiceTest {
         expectThat(eggNotFoundSlot.isCaptured).isTrue()
         expectThat(eggNotFoundSlot.captured.eggIdShell) isEqualTo otherEggId
         verify(exactly = 1) { eventRegistry.processEvents() }
+        verify(exactly = 1) { eggRepository.sync() }
+    }
+
+    @Test
+    fun `should discard existing protein`() {
+        // given
+        val eggId = shellOf("ProteinEgg")
+        val type = shellOf("typeA")
+        val structure = shellOf("structA")
+        val egg = createEggForTesting(withEggIdShell = eggId)
+        fakeCryptoProvider(instance = cryptoProvider)
+        // add a protein manually using encrypted shells
+        val encryptedType: EncryptedShell = cryptoProvider.encrypt(type)
+        val encryptedStructure: EncryptedShell = cryptoProvider.encrypt(structure)
+        egg.updateProtein(Slot.S1, encryptedType, encryptedStructure)
+        fakeEggRepository(instance = eggRepository, withEggs = listOf(egg))
+        expectThat(egg.proteins[Slot.S1.index()].isPresent).isTrue()
+
+        // when
+        passwordService.discardProtein(eggId, Slot.S1)
+
+        // then
+        expectThat(egg.proteins[Slot.S1.index()].isPresent).isNotEqualTo(true) // now empty
+        verify(exactly = 1) { eventRegistry.processEvents() }
+        verify(exactly = 1) { eggRepository.sync() }
+    }
+
+    @Test
+    fun `should not register EggNotFound when discarding non-existent protein`() {
+        // given
+        val eggId = shellOf("ProteinEggNoSlot")
+        val egg = createEggForTesting(withEggIdShell = eggId)
+        fakeCryptoProvider(instance = cryptoProvider)
+        fakeEggRepository(instance = eggRepository, withEggs = listOf(egg))
+        expectThat(egg.proteins[Slot.S2.index()].isPresent).isNotEqualTo(true)
+
+        // when
+        passwordService.discardProtein(eggId, Slot.S2)
+
+        // then
+        verify(exactly = 0) { eventRegistry.register(any<EggNotFound>()) }
+        verify(exactly = 1) { eventRegistry.processEvents() }
+        verify(exactly = 1) { eggRepository.sync() }
+    }
+
+    @Test
+    fun `should register EggNotFound when discarding protein of missing egg`() {
+        // given
+        val missingEggId = shellOf("MissingEgg")
+        fakeCryptoProvider(instance = cryptoProvider)
+        fakeEggRepository(instance = eggRepository) // empty repository
+        val eggNotFoundSlot = slot<EggNotFound>()
+
+        // when
+        passwordService.discardProtein(missingEggId, Slot.S3)
+
+        // then
+        verify { eventRegistry.register(capture(eggNotFoundSlot)) }
+        expectThat(eggNotFoundSlot.isCaptured).isTrue()
+        expectThat(eggNotFoundSlot.captured.eggIdShell) isEqualTo missingEggId
+        verify(exactly = 1) { eventRegistry.processEvents() }
+        verify(exactly = 1) { eggRepository.sync() }
     }
 }
