7aee56e5852fb81be9ba15adeb89daa93b3a1c42
Christian Pflugradt <110765548+christianpflugradt@users.noreply.github.com>
2026-05-27 10:33:09 +0200

    test(keystore): stabilize java 25 persistence test

--- src/test/kotlin/de/pflugradts/passbird/adapter/keystore/KeyStorePersistenceTest.kt
+++ src/test/kotlin/de/pflugradts/passbird/adapter/keystore/KeyStorePersistenceTest.kt
@@ -3,7 +3,6 @@ package de.pflugradts.passbird.adapter.keystore
 import de.pflugradts.passbird.application.util.SystemOperation
 import de.pflugradts.passbird.domain.model.shell.PlainShell.Companion.plainShellOf
 import de.pflugradts.passbird.domain.model.shell.Shell.Companion.shellOf
-import io.mockk.every
 import io.mockk.mockk
 import org.junit.jupiter.api.Test
 import strikt.api.expectThat
@@ -12,9 +11,16 @@ import strikt.assertions.isFalse
 import strikt.assertions.isNotEqualTo
 import strikt.assertions.isTrue
 import java.io.ByteArrayInputStream
+import java.io.InputStream
+import java.io.OutputStream
 import java.nio.file.Paths
 import java.security.Key
 import java.security.KeyStore
+import java.security.KeyStoreSpi
+import java.security.cert.Certificate
+import java.util.Collections
+import java.util.Date
+import java.util.Enumeration
 
 class KeyStorePersistenceTest {
 
@@ -32,10 +38,8 @@ class KeyStorePersistenceTest {
             override fun getFormat() = "RAW"
             override fun getEncoded() = loadedKeyBytes
         }
-        val keyStore = mockk<KeyStore>()
-        every { systemOperation.newInputStream(path) } returns ByteArrayInputStream(byteArrayOf())
-        every { keyStore.load(any(), any<CharArray>()) } returns Unit
-        every { keyStore.getKey(SECRET_ALIAS, any()) } returns loadedKey
+        val keyStore = TestKeyStore(loadedKey)
+        io.mockk.every { systemOperation.newInputStream(path) } returns ByteArrayInputStream(byteArrayOf())
 
         val actual = keyStorePersistence.loadKey(openKeyStore = { keyStore }, password = password, path = path)
 
@@ -44,5 +48,41 @@ class KeyStorePersistenceTest {
         expectThat(loadedKeyBytes.toList()) isNotEqualTo expectedKeyBytes.toList()
         expectThat(loadedKeyBytes.contentEquals(expectedKeyBytes)).isFalse()
         expectThat(password.toCharArray()) isNotEqualTo "Password".toCharArray()
+    }
+
+    private class TestKeyStore(loadedKey: Key) : KeyStore(TestKeyStoreSpi(loadedKey), null, "test")
+
+    private class TestKeyStoreSpi(private val loadedKey: Key) : KeyStoreSpi() {
+        override fun engineGetKey(alias: String?, password: CharArray?) = loadedKey
+
+        override fun engineGetCertificateChain(alias: String?): Array<Certificate>? = null
+
+        override fun engineGetCertificate(alias: String?): Certificate? = null
+
+        override fun engineGetCreationDate(alias: String?) = Date(0)
+
+        override fun engineSetKeyEntry(alias: String?, key: Key?, password: CharArray?, chain: Array<out Certificate>?) = Unit
+
+        override fun engineSetKeyEntry(alias: String?, key: ByteArray?, chain: Array<out Certificate>?) = Unit
+
+        override fun engineSetCertificateEntry(alias: String?, cert: Certificate?) = Unit
+
+        override fun engineDeleteEntry(alias: String?) = Unit
+
+        override fun engineAliases(): Enumeration<String> = Collections.emptyEnumeration()
+
+        override fun engineContainsAlias(alias: String?) = alias == SECRET_ALIAS
+
+        override fun engineSize() = 1
+
+        override fun engineIsKeyEntry(alias: String?) = alias == SECRET_ALIAS
+
+        override fun engineIsCertificateEntry(alias: String?) = false
+
+        override fun engineGetCertificateAlias(cert: Certificate?) = null
+
+        override fun engineStore(stream: OutputStream?, password: CharArray?) = Unit
+
+        override fun engineLoad(stream: InputStream?, password: CharArray?) = Unit
     }
 }
