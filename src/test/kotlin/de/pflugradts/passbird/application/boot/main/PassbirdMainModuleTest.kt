eacd7a0f01926387ef323bda0eeab13c62ec9986
Christian Pflugradt <110765548+christianpflugradt@users.noreply.github.com>
2026-05-29 21:19:27 +0200

    test(boot): prove multibinder completeness
    Closes #250

--- src/test/kotlin/de/pflugradts/passbird/application/boot/main/PassbirdMainModuleTest.kt
+++ src/test/kotlin/de/pflugradts/passbird/application/boot/main/PassbirdMainModuleTest.kt
@@ -3,58 +3,25 @@ package de.pflugradts.passbird.application.boot.main
 import com.google.inject.AbstractModule
 import com.google.inject.Guice
 import com.google.inject.util.Modules
+import com.tngtech.archunit.core.importer.ClassFileImporter
+import com.tngtech.archunit.core.importer.ImportOption.DoNotIncludeTests
 import de.pflugradts.passbird.INTEGRATION
 import de.pflugradts.passbird.application.PassbirdRunContext
-import de.pflugradts.passbird.application.commandhandling.handler.ChangeMasterPasswordCommandHandler
-import de.pflugradts.passbird.application.commandhandling.handler.ExportCommandHandler
-import de.pflugradts.passbird.application.commandhandling.handler.HelpCommandHandler
-import de.pflugradts.passbird.application.commandhandling.handler.ImportCommandHandler
-import de.pflugradts.passbird.application.commandhandling.handler.ListCommandHandler
-import de.pflugradts.passbird.application.commandhandling.handler.QuitCommandHandler
-import de.pflugradts.passbird.application.commandhandling.handler.RepeatLastCommandHandler
-import de.pflugradts.passbird.application.commandhandling.handler.SetInfoCommandHandler
-import de.pflugradts.passbird.application.commandhandling.handler.egg.CustomSetCommandHandler
-import de.pflugradts.passbird.application.commandhandling.handler.egg.DiscardCommandHandler
-import de.pflugradts.passbird.application.commandhandling.handler.egg.GetCommandHandler
-import de.pflugradts.passbird.application.commandhandling.handler.egg.OneTimeSetCommandHandler
-import de.pflugradts.passbird.application.commandhandling.handler.egg.RenameCommandHandler
-import de.pflugradts.passbird.application.commandhandling.handler.egg.SetCommandHandler
-import de.pflugradts.passbird.application.commandhandling.handler.egg.ViewCommandHandler
-import de.pflugradts.passbird.application.commandhandling.handler.favorite.AddFavoriteCommandHandler
-import de.pflugradts.passbird.application.commandhandling.handler.favorite.DiscardFavoriteCommandHandler
-import de.pflugradts.passbird.application.commandhandling.handler.favorite.FavoriteInfoCommandHandler
-import de.pflugradts.passbird.application.commandhandling.handler.favorite.GetFavoriteCommandHandler
-import de.pflugradts.passbird.application.commandhandling.handler.favorite.UseFavoriteCommandHandler
-import de.pflugradts.passbird.application.commandhandling.handler.favorite.ViewFavoriteCommandHandler
-import de.pflugradts.passbird.application.commandhandling.handler.memory.GetMemoryCommandHandler
-import de.pflugradts.passbird.application.commandhandling.handler.memory.MemoryInfoCommandHandler
-import de.pflugradts.passbird.application.commandhandling.handler.memory.UseMemoryCommandHandler
-import de.pflugradts.passbird.application.commandhandling.handler.memory.ViewMemoryCommandHandler
-import de.pflugradts.passbird.application.commandhandling.handler.nest.AddNestCommandHandler
-import de.pflugradts.passbird.application.commandhandling.handler.nest.DiscardNestCommandHandler
-import de.pflugradts.passbird.application.commandhandling.handler.nest.MoveToNestCommandHandler
-import de.pflugradts.passbird.application.commandhandling.handler.nest.SwitchNestCommandHandler
-import de.pflugradts.passbird.application.commandhandling.handler.nest.ViewNestCommandHandler
-import de.pflugradts.passbird.application.commandhandling.handler.protein.DiscardProteinCommandHandler
-import de.pflugradts.passbird.application.commandhandling.handler.protein.GetProteinCommandHandler
-import de.pflugradts.passbird.application.commandhandling.handler.protein.ProteinInfoCommandHandler
-import de.pflugradts.passbird.application.commandhandling.handler.protein.SetProteinCommandHandler
-import de.pflugradts.passbird.application.commandhandling.handler.protein.ViewProteinStructuresCommandHandler
-import de.pflugradts.passbird.application.commandhandling.handler.protein.ViewProteinTypesCommandHandler
-import de.pflugradts.passbird.application.eventhandling.ApplicationEventHandler
-import de.pflugradts.passbird.application.process.backup.BackupManager
-import de.pflugradts.passbird.application.process.exchange.ExportFileChecker
-import de.pflugradts.passbird.application.process.inactivity.InactivityHandlerScheduler
+import de.pflugradts.passbird.application.commandhandling.handler.CommandHandler
+import de.pflugradts.passbird.application.process.Finalizer
+import de.pflugradts.passbird.application.process.Initializer
 import de.pflugradts.passbird.application.toDirectory
 import de.pflugradts.passbird.domain.model.slot.Slot
-import de.pflugradts.passbird.domain.service.eventhandling.DomainEventHandler
+import de.pflugradts.passbird.domain.service.eventhandling.EventHandler
 import de.pflugradts.passbird.domain.service.password.encryption.CryptoProvider
 import io.mockk.mockk
 import org.junit.jupiter.api.Tag
 import org.junit.jupiter.api.Test
 import strikt.api.expectThat
 import strikt.assertions.isA
+import strikt.assertions.isEqualTo
 import strikt.assertions.isSameInstanceAs
+import java.lang.reflect.Modifier
 
 @Tag(INTEGRATION)
 class PassbirdMainModuleTest {
@@ -68,18 +35,13 @@ class PassbirdMainModuleTest {
         // then
         expectThat(actual.bootable).isA<PassbirdApplication>()
         expectThat(actual.runContext) isSameInstanceAs runContext
-        actual.commandHandlers.forEachIndexed { index, commandHandler ->
-            expectThat(commandHandler::class.java) isSameInstanceAs expectedCommandHandlers[index]
-        }
-        actual.eventHandlers.forEachIndexed { index, eventHandler ->
-            expectThat(eventHandler::class.java) isSameInstanceAs expectedEventHandlers[index]
-        }
-        actual.initializers.forEachIndexed { index, initializer ->
-            expectThat(initializer::class.java) isSameInstanceAs expectedInitializers[index]
-        }
-        actual.finalizers.forEachIndexed { index, finalizer ->
-            expectThat(finalizer::class.java) isSameInstanceAs expectedFinalizers[index]
-        }
+        expectThat(actual.commandHandlers.implementationClasses()) isEqualTo expectedMultibinderClasses(CommandHandler::class.java)
+        expectThat(actual.eventHandlers.implementationClasses()) isEqualTo expectedMultibinderClasses(
+            EventHandler::class.java,
+            CommandHandler::class.java,
+        )
+        expectThat(actual.initializers.implementationClasses()) isEqualTo expectedMultibinderClasses(Initializer::class.java)
+        expectThat(actual.finalizers.implementationClasses()) isEqualTo expectedMultibinderClasses(Finalizer::class.java)
     }
 
     class PassbirdTestModule : AbstractModule() {
@@ -89,54 +51,18 @@ class PassbirdMainModuleTest {
     }
 
     companion object {
-        private val expectedCommandHandlers = listOf(
-            AddFavoriteCommandHandler::class.java,
-            AddNestCommandHandler::class.java,
-            MoveToNestCommandHandler::class.java,
-            CustomSetCommandHandler::class.java,
-            OneTimeSetCommandHandler::class.java,
-            ChangeMasterPasswordCommandHandler::class.java,
-            DiscardCommandHandler::class.java,
-            DiscardFavoriteCommandHandler::class.java,
-            DiscardNestCommandHandler::class.java,
-            DiscardProteinCommandHandler::class.java,
-            ExportCommandHandler::class.java,
-            FavoriteInfoCommandHandler::class.java,
-            GetFavoriteCommandHandler::class.java,
-            GetCommandHandler::class.java,
-            GetMemoryCommandHandler::class.java,
-            GetProteinCommandHandler::class.java,
-            HelpCommandHandler::class.java,
-            ImportCommandHandler::class.java,
-            ListCommandHandler::class.java,
-            MemoryInfoCommandHandler::class.java,
-            ProteinInfoCommandHandler::class.java,
-            QuitCommandHandler::class.java,
-            RepeatLastCommandHandler::class.java,
-            RenameCommandHandler::class.java,
-            SetCommandHandler::class.java,
-            SetInfoCommandHandler::class.java,
-            SetProteinCommandHandler::class.java,
-            SwitchNestCommandHandler::class.java,
-            UseFavoriteCommandHandler::class.java,
-            UseMemoryCommandHandler::class.java,
-            ViewCommandHandler::class.java,
-            ViewFavoriteCommandHandler::class.java,
-            ViewMemoryCommandHandler::class.java,
-            ViewNestCommandHandler::class.java,
-            ViewProteinStructuresCommandHandler::class.java,
-            ViewProteinTypesCommandHandler::class.java,
-        )
-        private val expectedEventHandlers = listOf(
-            ApplicationEventHandler::class.java,
-            DomainEventHandler::class.java,
-        )
-        private val expectedInitializers = listOf(
-            ExportFileChecker::class.java,
-            InactivityHandlerScheduler::class.java,
-        )
-        private val expectedFinalizers = listOf(
-            BackupManager::class.java,
-        )
+        private const val ROOT = "de.pflugradts.passbird"
+        private val productionClasses = ClassFileImporter().withImportOption(DoNotIncludeTests()).importPackages(ROOT)
+
+        private fun expectedMultibinderClasses(type: Class<*>, vararg excludedTypes: Class<*>) = productionClasses
+            .map { Class.forName(it.name) }
+            .filter { it.isConcreteImplementationOf(type) }
+            .filterNot { candidate -> excludedTypes.any { it.isAssignableFrom(candidate) } }
+            .toSet()
+
+        private fun Class<*>.isConcreteImplementationOf(type: Class<*>) =
+            type.isAssignableFrom(this) && !isInterface && !Modifier.isAbstract(modifiers)
     }
 }
+
+private fun <T : Any> Iterable<T>.implementationClasses(): Set<Class<*>> = map { it::class.java }.toSet()
