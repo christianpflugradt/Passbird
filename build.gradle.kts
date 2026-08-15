4d950bf8d171115ad7283c9feb497f36eb4fd9ca
Christian Pflugradt <110765548+christianpflugradt@users.noreply.github.com>
2026-08-15 13:29:31 +0200

    fix(deps): address owasp dependency violations

--- build.gradle.kts
+++ build.gradle.kts
@@ -40,7 +40,7 @@ detekt {
     source.setFrom("src/main/kotlin", "src/test/kotlin")
 }
 
-val jacksonVersion = "2.21.4"
+val jacksonVersion = "2.22.1"
 
 val archunitVersion = "1.4.2"
 val junitPlatformVersion = "6.1.0"
